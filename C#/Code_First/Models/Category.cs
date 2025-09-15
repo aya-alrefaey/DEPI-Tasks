4a37731165f8b69b79de6cd373f67898544252bd
aya-alrefaey <ayaalazab164@gmail.com>
2025-09-15 23:04:46 +0300

    Add files via upload

---
+++ C#/Code_First/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_First.Models
+{
+    internal class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ICollection<News> news { get; } = new List<News>();
+    }
+}
