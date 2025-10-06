ba512fa86988309f2eeae5f5510d5ea721b79edc
aya-alrefaey <ayaalazab164@gmail.com>
2025-10-06 20:58:53 +0300

    Add files via upload

---
+++ first mvc/first mvc/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace first_mvc.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
