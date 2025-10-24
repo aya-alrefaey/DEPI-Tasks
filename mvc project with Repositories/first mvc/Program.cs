a6c5a4a7b0d25f8bc005d8ee0346bb82c97c6a84
aya-alrefaey <ayaalazab164@gmail.com>
2025-10-24 03:25:36 +0300

    Add files via upload

---
+++ mvc project with Repositories/first mvc/Program.cs
@@ -0,0 +1,40 @@
+using first_mvc.Repositories;
+
+namespace first_mvc
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddScoped<ICustomerRepo,CustomersRepository>();
+            builder.Services.AddScoped<ICategoriesRepo, CategoriesRepository>();
+            builder.Services.AddScoped<IProductRepo, ProductsRepository>();
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
